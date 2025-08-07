0878c8b6e5d28adf8ccd3d2cbb12358bab3fed24
MarioMastr <ashrmoha11@gmail.com>
2025-08-07 19:35:12 +0300

    of course this breaks windows as well

--- src/shaders.hpp
+++ src/shaders.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
+#include <Geode/platform/cplatform.h>
+
+#ifdef GEODE_IS_MACOS
 #include <OpenGL/OpenGL.h>
+#endif
 
 extern const GLchar* g_outlineShaderFragment;
 extern const GLchar* g_outlineShaderVertex;
